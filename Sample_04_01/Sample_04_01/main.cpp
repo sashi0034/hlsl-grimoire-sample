007b2537291f426416c91aad3ebb508cb6af6620
sashi <satoshi3104.nishi@gmail.com>
2025-02-01 16:32:46 +0900

    4.1

--- Sample_04_01/Sample_04_01/main.cpp
+++ Sample_04_01/Sample_04_01/main.cpp
@@ -13,11 +13,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // ここから初期化を行うコードを記述する
     //////////////////////////////////////
 
-    g_camera3D->SetPosition({ 0.0f, 50.0f, 100.0f });
+    g_camera3D->SetPosition({0.0f, 50.0f, 100.0f});
 
     // step-1 3Dモデルをロードするための情報を設定する
+    ModelInitData modelInitData;
+    modelInitData.m_tkmFilePath = "Assets/modelData/sample.tkm";
+    modelInitData.m_fxFilePath = "Assets/shader/sample.after.fx";
 
     // step-2 初期化情報を使ってモデル表示処理を初期化する
+    Model charaModel;
+    charaModel.Init(modelInitData);
 
     //////////////////////////////////////
     // 初期化を行うコードを書くのはここまで！！！
@@ -35,6 +40,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         //////////////////////////////////////
 
         // step-3 モデルのドローコールを実行する
+        charaModel.Draw(renderContext);
 
         //////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
